6616b129c6b2693e6fb0b8adced5d6f09d910579
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-01-29 13:27:47 +0330

    new system factors

--- script.js
+++ script.js
@@ -1,106 +1,117 @@
-// ------- Phone Functions ------- //
-// Step1 : create a variable for phone color and get the element by id 
 let selectedProduct = {};
 
-function productSystem(){
-    let phoneColor = document.getElementById('phone-img')
+function productSystem(productName, productInfo, productColor, productPrice){
+    selectedProduct = {
+        name: productName,
+        info: productInfo,
+        color: productColor,
+        price: productPrice
+    };
+}
 
-    let colorYellowPhone =  document.getElementById('phone-yellow')
-    let colorBulePhone =  document.getElementById('phone-blue')
-    let colorPinkPhone =  document.getElementById('phone-pink')
+function addToInvoice() {
+    let invoice = document.getElementById('invoice');
+    if (selectedProduct.name && selectedProduct.info && selectedProduct.color && selectedProduct.price) {
+        let item = document.createElement('div');
+        item.innerHTML = `<p>${selectedProduct.name} - ${selectedProduct.color} - ${selectedProduct.price}</p>`;
+        invoice.appendChild(item);
+    } else {
+        alert("plaese select color product") 
+    }
+}
 
+
+// Phone Functions
+function phoneSystem() {
+    let phoneColor = document.getElementById('phone-img');
+    let phonePriceElement = document.getElementById("phone-price");
+
+    let colorYellowPhone = document.getElementById('phone-yellow');
+    let colorBluePhone = document.getElementById('phone-blue');
+    let colorPinkPhone = document.getElementById('phone-pink');
 
     colorYellowPhone.addEventListener('click', function(){
         phoneColor.src = "./images/apple-iphone-15-yellow.jpg";
-        document.getElementById("phone-price").innerHTML = "Price: $1000"; 
+        phonePriceElement.innerHTML = "Price: $1000";
+        productSystem('iPhone 15', '256 GB - 8GB RAM', 'Yellow', '$1000');
+    });
 
-    })
-
-    colorBulePhone.addEventListener('click', function(){
+    colorBluePhone.addEventListener('click', function(){
         phoneColor.src = "./images/apple-iphone-15-blue.jpg";
-        document.getElementById("phone-price").innerHTML = "Price: $950";
-
-    })
+        phonePriceElement.innerHTML = "Price: $950";
+        productSystem('iPhone 15', '256 GB - 8GB RAM', 'Blue', '$950');
+    });
 
     colorPinkPhone.addEventListener('click', function(){
         phoneColor.src = "./images/apple-iphone-15-pink.jpg";
-        document.getElementById("phone-price").innerHTML = "Price: $1500";
+        phonePriceElement.innerHTML = "Price: $1500";
+        productSystem('iPhone 15', '256 GB - 8GB RAM', 'Pink', '$1500');
+    });
+}
 
-    })
-    // ----------------------------------------------------------------------
+// Car Functions
+function carSystem() {
+    let carColor = document.getElementById('car-img');
+    let carPriceElement = document.getElementById("car-price");
 
-    let watchColor = document.getElementById('watch-img')
-
-    let colorWatchStarlight = document.getElementById('watch-starlight')
-    let colorWatchLightpink = document.getElementById('watch-lightpink')
-    let colorWatchWinterblue = document.getElementById('watch-winterblue')
-
-    colorWatchStarlight.addEventListener('click', function(){
-        watchColor.src = "./images/starlight-watch-aluminum-midnight.jpg";
-        document.getElementById("watch-price").innerHTML = "Price: $500"; 
-    })
-
-    colorWatchLightpink.addEventListener('click', function(){
-        watchColor.src = "./images/lightpink-watch-aluminum-midnight.jpg";
-        document.getElementById("watch-price").innerHTML = "Price: $650";
-    })
-
-    colorWatchWinterblue.addEventListener('click', function(){
-        watchColor.src = "./images/winterblue-watch-aluminum-midnight.jpg";
-        document.getElementById("watch-price").innerHTML = "Price: $400";
-    })
-
-    // ----------------------------------------------------------------------
-    let carColor = document.getElementById('car-img')
-
-    let colorCarBlack = document.getElementById('car-black')
-    let colorCarJupiterRed = document.getElementById('car-jupiterRed')
-    let colorCarClapolarwhite = document.getElementById('car-polarWhite')
-    let colorCarClacavansiteBlue = document.getElementById('car-cavansiteBlue')
+    let colorCarBlack = document.getElementById('car-black');
+    let colorCarJupiterRed = document.getElementById('car-jupiterRed');
+    let colorCarPolarWhite = document.getElementById('car-polarWhite');
+    let colorCarCavansiteBlue = document.getElementById('car-cavansiteBlue');
 
     colorCarBlack.addEventListener('click', function(){
         carColor.src = "./images/clacosmosblack.jpg";
-        document.getElementById("car-price").innerHTML = "Price: $400,000";
-    })
+        carPriceElement.innerHTML = "Price: $400,000";
+        productSystem('Mercedes-Benz CLA', 'Description', 'Black', '$400,000');
+    });
 
     colorCarJupiterRed.addEventListener('click', function(){
         carColor.src = "./images/clajupiterred.jpg";
-        document.getElementById("car-price").innerHTML = "Price: $550,000";
-    })
+        carPriceElement.innerHTML = "Price: $550,000";
+        productSystem('Mercedes-Benz CLA', 'Description', 'Jupiter Red', '$550,000');
+    });
 
-    colorCarClapolarwhite.addEventListener('click', function(){
+    colorCarPolarWhite.addEventListener('click', function(){
         carColor.src = "./images/clapolarwhite.jpg";
-        document.getElementById("car-price").innerHTML = "Price: $550,000";
-    })
-    
-    colorCarClacavansiteBlue.addEventListener('click', function(){
+        carPriceElement.innerHTML = "Price: $550,000";
+        productSystem('Mercedes-Benz CLA', 'Description', 'Polar White', '$550,000');
+    });
+
+    colorCarCavansiteBlue.addEventListener('click', function(){
         carColor.src = "./images/clacavansiteblue.jpg";
-        document.getElementById("car-price").innerHTML = "Price: $600,000";
-    })
+        carPriceElement.innerHTML = "Price: $600,000";
+        productSystem('Mercedes-Benz CLA', 'Description', 'Cavansite Blue', '$600,000');
+    });
 }
 
+// Watch Functions
+function watchSystem() {
+    let watchColor = document.getElementById('watch-img');
+    let watchPriceElement = document.getElementById("watch-price");
 
+    let colorWatchStarlight = document.getElementById('watch-starlight');
+    let colorWatchLightpink = document.getElementById('watch-lightpink');
+    let colorWatchWinterblue = document.getElementById('watch-winterblue');
 
+    colorWatchStarlight.addEventListener('click', function(){
+        watchColor.src = "./images/starlight-watch-aluminum-midnight.jpg";
+        watchPriceElement.innerHTML = "Price: $500";
+        productSystem('Apple Watch', 'SE 2022 Aluminum Case 40mm', 'Starlight', '$500');
+    });
 
+    colorWatchLightpink.addEventListener('click', function(){
+        watchColor.src = "./images/lightpink-watch-aluminum-midnight.jpg";
+        watchPriceElement.innerHTML = "Price: $650";
+        productSystem('Apple Watch', 'SE 2022 Aluminum Case 40mm', 'Light Pink', '$650');
+    });
 
-
-
-function product(){
-    phone = {
-        'namePhone': document.getElementById("namePhone"),
-        'infoPhone': document.getElementById("infoPhone"),
-        'colorPhone': document.getElementById("colorPhone"),
-        'pricePhonee': document.getElementById("phone-price"),
-
-    }
-
+    colorWatchWinterblue.addEventListener('click', function(){
+        watchColor.src = "./images/winterblue-watch-aluminum-midnight.jpg";
+        watchPriceElement.innerHTML = "Price: $400";
+        productSystem('Apple Watch', 'SE 2022 Aluminum Case 40mm', 'Winter Blue', '$400');
+    });
 }
-product()
 
-function factors(){
-
-}
-
-
-
-
+phoneSystem();
+carSystem();
+watchSystem();
