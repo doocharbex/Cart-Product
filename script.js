d70ebece1a62d7c07516859392fb6e54686d3181
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-01-29 11:52:00 +0330

    new system change info product
    By implementing a system to change the value of a card
    Previous system: There was a function for each color change button, but this code was optimized

--- script.js
+++ script.js
@@ -1,70 +1,106 @@
 // ------- Phone Functions ------- //
 // Step1 : create a variable for phone color and get the element by id 
-let phoneColor = document.getElementById('phone-img')
+let selectedProduct = {};
 
-// Step2 : create a function for phone color changing
-function changePhoneColor() {
-    // Step3 : change the image of the phone as the user clicks on each button
-    phoneColor.src = "./images/apple-iphone-15-yellow.jpg";
-    // Step4 : change the price for each color
-    document.getElementById("phone-price").innerHTML = "Price: $1000"; 
+function productSystem(){
+    let phoneColor = document.getElementById('phone-img')
+
+    let colorYellowPhone =  document.getElementById('phone-yellow')
+    let colorBulePhone =  document.getElementById('phone-blue')
+    let colorPinkPhone =  document.getElementById('phone-pink')
+
+
+    colorYellowPhone.addEventListener('click', function(){
+        phoneColor.src = "./images/apple-iphone-15-yellow.jpg";
+        document.getElementById("phone-price").innerHTML = "Price: $1000"; 
+
+    })
+
+    colorBulePhone.addEventListener('click', function(){
+        phoneColor.src = "./images/apple-iphone-15-blue.jpg";
+        document.getElementById("phone-price").innerHTML = "Price: $950";
+
+    })
+
+    colorPinkPhone.addEventListener('click', function(){
+        phoneColor.src = "./images/apple-iphone-15-pink.jpg";
+        document.getElementById("phone-price").innerHTML = "Price: $1500";
+
+    })
+    // ----------------------------------------------------------------------
+
+    let watchColor = document.getElementById('watch-img')
+
+    let colorWatchStarlight = document.getElementById('watch-starlight')
+    let colorWatchLightpink = document.getElementById('watch-lightpink')
+    let colorWatchWinterblue = document.getElementById('watch-winterblue')
+
+    colorWatchStarlight.addEventListener('click', function(){
+        watchColor.src = "./images/starlight-watch-aluminum-midnight.jpg";
+        document.getElementById("watch-price").innerHTML = "Price: $500"; 
+    })
+
+    colorWatchLightpink.addEventListener('click', function(){
+        watchColor.src = "./images/lightpink-watch-aluminum-midnight.jpg";
+        document.getElementById("watch-price").innerHTML = "Price: $650";
+    })
+
+    colorWatchWinterblue.addEventListener('click', function(){
+        watchColor.src = "./images/winterblue-watch-aluminum-midnight.jpg";
+        document.getElementById("watch-price").innerHTML = "Price: $400";
+    })
+
+    // ----------------------------------------------------------------------
+    let carColor = document.getElementById('car-img')
+
+    let colorCarBlack = document.getElementById('car-black')
+    let colorCarJupiterRed = document.getElementById('car-jupiterRed')
+    let colorCarClapolarwhite = document.getElementById('car-polarWhite')
+    let colorCarClacavansiteBlue = document.getElementById('car-cavansiteBlue')
+
+    colorCarBlack.addEventListener('click', function(){
+        carColor.src = "./images/clacosmosblack.jpg";
+        document.getElementById("car-price").innerHTML = "Price: $400,000";
+    })
+
+    colorCarJupiterRed.addEventListener('click', function(){
+        carColor.src = "./images/clajupiterred.jpg";
+        document.getElementById("car-price").innerHTML = "Price: $550,000";
+    })
+
+    colorCarClapolarwhite.addEventListener('click', function(){
+        carColor.src = "./images/clapolarwhite.jpg";
+        document.getElementById("car-price").innerHTML = "Price: $550,000";
+    })
+    
+    colorCarClacavansiteBlue.addEventListener('click', function(){
+        carColor.src = "./images/clacavansiteblue.jpg";
+        document.getElementById("car-price").innerHTML = "Price: $600,000";
+    })
 }
-function changePhoneColor2() {
-    phoneColor.src = "./images/apple-iphone-15-blue.jpg";
-    document.getElementById("phone-price").innerHTML = "Price: $950";
+
+
+
+
+
+
+
+function product(){
+    phone = {
+        'namePhone': document.getElementById("namePhone"),
+        'infoPhone': document.getElementById("infoPhone"),
+        'colorPhone': document.getElementById("colorPhone"),
+        'pricePhonee': document.getElementById("phone-price"),
+
+    }
+
 }
-function changePhoneColor3() {
-    phoneColor.src = "./images/apple-iphone-15-pink.jpg";
-    document.getElementById("phone-price").innerHTML = "Price: $1500";
-}
+product()
 
+function factors(){
 
-
-// ------- Watch Functions ------- //
-// Step1 : create a variable for watch color and get the element by id 
-let watchColor = document.getElementById('watch-img')
-
-// Step2 : create a function for watch color changing
-function changeWatchColor() {
-    // Step3 : change the image of the watch as the user clicks on each button
-    watchColor.src = "./images/starlight-watch-aluminum-midnight.jpg";
-    // Step4 : change the price for each color
-    document.getElementById("watch-price").innerHTML = "Price: $500"; 
-}
-
-function changeWatchColor2() {
-    watchColor.src = "./images/lightpink-watch-aluminum-midnight.jpg";
-    document.getElementById("watch-price").innerHTML = "Price: $650";
-}
-function changeWatchColor3() {
-    watchColor.src = "./images/winterblue-watch-aluminum-midnight.jpg";
-    document.getElementById("watch-price").innerHTML = "Price: $400";
 }
 
 
 
-// ------- Car Functions ------- //
-// Step1 : create a variable for car color and get the element by id 
-let carColor = document.getElementById('car-img')
 
-// Step2 : create a function for car color changing
-function changeCarColor() {
-    // Step3 : change the image of the car as the user clicks on each button
-    carColor.src = "./images/clacosmosblack.jpg";
-    // Step4 : change the price for each color
-    document.getElementById("car-price").innerHTML = "Price: $500,000"; 
-}
-
-function changeCarColor2() {
-    carColor.src = "./images/clajupiterred.jpg";
-    document.getElementById("car-price").innerHTML = "Price: $550,000";
-}
-function changeCarColor3() {
-    carColor.src = "./images/clapolarwhite.jpg";
-    document.getElementById("car-price").innerHTML = "Price: $470,000";
-}
-
-function changeCarColor4() {
-    carColor.src = "./images/clacavansiteblue.jpg";
-    document.getElementById("car-price").innerHTML = "Price: $600,000";
-}
